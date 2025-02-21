a6ca024ec47769cb5f6878b0e7010fd1226055d5
Yariangel Aray <gabrielamunar4@gmail.com>
2025-02-21 08:06:57 -0500

    ejercicio 2

---
+++ Ejercicio 2/Ejercicio2.js
@@ -0,0 +1,63 @@
+//Importamos las funciones que utilizaremos
+import { pedirNumero, pedirLetra, sumarNumeros, contarPares, contarPrimos, promedioPrimos, promedioPares } from "./Modulo2.js";
+
+//Pedimos la cantidad de números, llamando a la función que nos válida que sea un número.
+let cantNumeros = pedirNumero("Ingrese la cantidad de números que desea ingresar.");
+let numeros = []; // Creamos un arreglo vacio para almacenr los números
+
+// for que nos ayudará a pedir los números de números según sea la cantidad
+for (let i = 0; i < cantNumeros; i++) {
+  numeros.push(pedirNumero(`Ingrese el número ${i+1}, por favor:`));  //pedimos el número agregandolo a su vez al arreglo
+}
+
+// Variable booleana que nos ayudará a controlar el bucle dowhile y nos permitirá salir de el
+let continuar = true;
+do {
+  // Menú de opciones, llamamos a una función que nos válida que la letra ingresada sea una única letra minuscula
+  let opc = pedirLetra(`MENÚ:
+    a. Sumar los números
+    b. Contar los números primos
+    c. Contar los números pares
+    d. Calcular el promedio de los números primos
+    e. Calcular el promedio de números pares
+    f. Salir del programa.
+    \nIngrese su opción: `);
+  
+  // Switch que nos permitirá realizar una acción según sea el caso.
+  switch (opc) {
+    case "a": // Sumamos los números con ayuda de una función que, al enviarle como argumento el arreglo de números, nos devuelve la suma total.
+      let suma = sumarNumeros(numeros);
+      alert("La suma de todos los números es: " + suma);
+      break;
+    
+    case "b": //Contamos la cantidad de números primos con ayuda de una función que, al enviarle como argumento el arreglo de números, nos devuelve la cantidad total de números primos.
+      let numPrimos = contarPrimos(numeros);
+      alert("La cantidad de números primos es: " + numPrimos);
+      break;
+    
+    case "c"://Contamos la cantidad de números pares con ayuda de una función que, al enviarle como argumento el arreglo de números, nos devuelve la cantidad total de números pares.
+      let numPares = contarPares(numeros);
+      alert("La cantidad de números primos es: " + numPares);
+      break;
+    
+    case "d": //Calculamos el promedio de números primos con ayuda de una función que, al enviarle como argumento el arreglo de números, nos devuelve el promedio de estos mismos.
+      let promPrimos = promedioPrimos(numeros);
+      alert("El promedio de los numeros primos es: " + promPrimos);
+      break;
+    
+    case "e": //Calculamos el promedio de números pares con ayuda de una función que, al enviarle como argumento el arreglo de números, nos devuelve el promedio de estos mismos.
+      let promPares = promedioPares(numeros);
+      alert("El promedio de los numeros primos es: " + promPares);
+      break;
+    
+    case "f": //El programa termina si se ingresa una letra "f"
+      alert("Programa terminado, muchas gracias por usarlo.");
+      continuar = false; // cambiamos el valor de la variable a false, lo que nos permitirá romper el ciclo
+      break;
+    
+    default: // Se muestra mensaje de error para cuando se ingrese una opción inválida.
+      alert("ERROR: Opción inválida.")
+      break;
+  }
+  
+} while (continuar);
---
+++ Ejercicio 2/Modulo2.js
@@ -0,0 +1,86 @@
+// función para pedir un número, recibe como parametro el mensaje que se mostrará, y válida que sea un número utilizando una expresión regular.
+export let pedirNumero = (mensaje) => {
+  let RegExp = /^\d+$/;  
+  do {
+    let num = prompt(mensaje);
+
+    if (!RegExp.test(num)) alert("ERROR: Ingrese solo valores númericos positivos.");
+    else return parseInt(num);
+    
+  } while (true);
+}
+
+// función para pedir una letra, recibe como parametro el mensaje que se mostrará, y válida que sea una letra minuscula utilizando una expresión regular.
+export let pedirLetra = (mensaje) => {
+  let RegExp = /^[a-f]$/;  
+  do {
+    let letra = prompt(mensaje);
+  
+    if (!RegExp.test(letra)) alert("ERROR: Ingrese solo una letra minúscula.");
+    else return letra;
+
+  } while (true);
+
+}
+
+// Función que retorna la suma de todos los números de un arreglo recibiendolo como parámetro
+export let sumarNumeros = (numeros) => {
+  let suma = 0;
+  for (const num of numeros) suma += num;
+  return suma;
+}
+
+// Función que evalua un número y retorna un valor booleano dependiendo si es primo o no.
+let esPrimo = (numero) => {
+  if (numero <= 1) return false;
+  for (let i = 2; i < numero; i++) 
+    if (numero % i == 0) return false;
+
+  return true;
+}
+
+// Función que retorna la cantidad de números primos en un arreglo
+export let contarPrimos = (numeros) => {
+  let cantNumPrimos = 0;
+
+  for (const num of numeros) {
+    if (esPrimo(num)) cantNumPrimos++; 
+  }
+
+  return cantNumPrimos;
+}
+
+//función que retorna la cantidad de números pares en un arreglo
+export let contarPares = (numeros) => {
+  let cantNumPares = 0;
+
+  for (const num of numeros) {
+    if (num % 2 == 0) cantNumPares++;
+  }
+
+  return cantNumPares;
+}
+
+//funció que retorna el promedio de los números primos de un arreglo
+export let promedioPrimos = (numeros) => {
+  let cantNum = contarPrimos(numeros);
+  let suma = 0;
+
+  for (const num of numeros) {
+    if (esPrimo(num)) suma += num;
+  }
+
+  return suma/cantNum;
+}
+
+//función que retorna el promedio de los números pares de un arreglo
+export let promedioPares = (numeros) => {
+  let cantNum = contarPares(numeros);
+  let suma = 0;
+
+  for (const num of numeros) {
+    if (num % 2 == 0) suma += num;
+  }
+
+  return suma/cantNum;
+}
